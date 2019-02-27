b1476adf5b5e8086d846bedf657b42dd2d4507bf
Manuel Birba <manuelbirba@hotmail.fr>
2019-02-27 20:26:41 +0100

    [SERVER] review

--- server/app.js
+++ server/app.js
@@ -1,28 +1,50 @@
 const express = require('express')
 const http = require('http')
-
 const app = express()
-// const adress = 'http://localhost:'
 const config = { port: Number(process.env.PORT || 8000) }
+
+const bodyParser = require('body-parser')
 
 /*****************************************        Begin Start listening    *****************************************/
 http.Server(app).listen(config.port, function() {
-  console.log(`API server started at port ${config.port}`)
+  console.log(`API server started at port ${config.port} !!!`)
 })
 /*****************************************        End  Start listening    *****************************************/
 
 /*****************************************        Begin ROUTING    *****************************************/
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', handleHome)
 app.get('/wrongurl', handleBadUrl)
+app.get('/me', me)
+app.post('/shorturl', shorturl)
 /*****************************************        End ROUTING    *****************************************/
 
 /*****************************************        Begin Handle ROUTING    *****************************************/
 function handleHome(req, res) {
-  // res.sendFile(__dirname + '/index.html')
   res.status(200).json({ success: 'welcome to Amber-URL-Shortner !' })
 }
-
 function handleBadUrl(req, res) {
   res.status(400).json({ error: 'Is not a valid url' })
+}
+function shorturl(req, res) {
+  if (!req.body) {
+    console.log('!req.body')
+    res.redirect('/wrongurl')
+  }
+  console.log('req.body')
+  console.log(req.body)
+  res.status(200).json({
+    url: req.body,
+    shortURL: 'URL shortener',
+  })
+}
+function me(req, res) {
+  res.status(200).json({
+    success: "It's Manuel Birba API",
+    date: '26/02/2019',
+    goal: 'succed Amber Test',
+  })
 }
 /*****************************************        End Handle ROUTING    *****************************************/
