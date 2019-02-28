0c1102f1441b020e0210c216839709b005b9c6fb
Manuel Birba <manuelbirba@hotmail.fr>
2019-02-28 03:30:31 +0100

    [SERVER] add route to query db

--- server/app.js
+++ server/app.js
@@ -9,8 +9,13 @@ const bodyParser = require('body-parser')
 // console.log(urlshortener)
 
 const MongoClient = require('mongodb').MongoClient
-const assert = require('assert') //librairy
-const uri = 'mongodb+srv://BirMan:Rei7quie@mikutanodb-obhnq.mongodb.net/' //uri to my database
+const assert = require('assert') //library
+
+/**************************************************************************************/
+//uri to my database in reel projet this information is host in config an external file
+const uri = 'mongodb+srv://BirMan:Rei7quie@mikutanodb-obhnq.mongodb.net/'
+/**************************************************************************************/
+
 const client = new MongoClient(uri, { useNewUrlParser: true })
 client.connect((err, client) => {
   assert.equal(null, err)
@@ -59,6 +64,7 @@ app.get('/', handleHome)
 app.get('/wrongurl', handleBadUrl)
 app.get('/me', me)
 app.post('/shorturl', shorturl)
+app.get('/adddocuments', adddocuments)
 /*****************************************        End ROUTING    *****************************************/
 
 /*****************************************        Begin Handle ROUTING    *****************************************/
@@ -85,6 +91,43 @@ function me(req, res) {
     success: "It's Manuel Birba API",
     date: '26/02/2019',
     goal: 'succed Amber Test',
+  })
+}
+function adddocuments(req, res) {
+  client.connect((err, client) => {
+    assert.equal(null, err)
+    const collection = client.db('ambershortner').collection('personnes')
+    //Insert 5 last shortner url
+    collection.insertMany([
+      {
+        url: 'https://github.com/BirbaManuel/url-shortener',
+        urlshortener: 'https://bit.ly/2TlNJto',
+      },
+      {
+        url:
+          'https://gist.github.com/BirbaManuel/ad443b0e5744b3e0015133234e12835c',
+        urlshortener: 'https://bit.ly/2BTPPac',
+      },
+      {
+        url: 'https://amber-url-shortner.herokuapp.com/wrongurl',
+        urlshortener: 'https://bit.ly/2IPTtb4',
+      },
+      {
+        url: 'https://amber-url-shortner.herokuapp.com/',
+        urlshortener: 'https://bit.ly/2GO6yQ3',
+      },
+      {
+        url:
+          'https://5c7589b20ebb4b7b60ffbeb2--serene-aryabhata-8aefe1.netlify.com/',
+        urlshortener: 'https://bit.ly/2IPTUCe',
+      },
+    ])
+
+    // perform actions on the collection object
+    client.close()
+  })
+  res.status(200).json({
+    success: '5 last shortner URL insered in Database !!!',
   })
 }
 /*****************************************        End Handle ROUTING    *****************************************/
