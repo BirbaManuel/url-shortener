0587d2fe90c0de931e8a9ae0fe7ca1e37585a26f
Manuel Birba <manuelbirba@hotmail.fr>
2019-02-28 14:17:16 +0100

    [SERVER] beta version of code <=> decode url

---
+++ server/urlshortener.js
@@ -0,0 +1,26 @@
+const data =
+  'https://5c7589b20ebb4b7b60ffbeb2--serene-aryabhata-8aefe1.netlify.com/'
+const crypto = require('crypto')
+
+const urlshort = {
+  short: function(url) {
+    let code = crypto
+      .createHash('md5')
+      .update(data)
+      .digest('hex')
+    return code
+  },
+  enhanced: function(url) {
+    if (url === '46aaab2081983c4553980869b98d6dd0')
+      return 'https://5c7589b20ebb4b7b60ffbeb2--serene-aryabhata-8aefe1.netlify.com/'
+    // let code = crypto
+    //   .createHash('md5')
+    //   .update(data)
+    //   .digest('hex')
+    return data
+  },
+}
+console.log(urlshort.short(data))
+console.log(urlshort.enhanced(urlshort.short(data)))
+
+module.exports = urlshort
