c94e8253b6bf3d31cc78747be640cefccd89cc24
Manuel Birba <manuelbirba@hotmail.fr>
2019-02-28 20:14:37 +0100

    [SERVER] setup better config for Atlas MongoDB

--- server/config.js
+++ server/config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  uri: 'mongodb+srv://BirMan:Rei7quie@mikutanodb-obhnq.mongodb.net/',
+  // uri: 'mongodb+srv://BirMan:Rei7quie@mikutanodb-obhnq.mongodb.net/',
+  uri:
+    'mongodb+srv://BirMan:Rei7quie@mikutanodb-obhnq.mongodb.net/ambershortner?ssl=true&authSource=admin',
 }
--- server/models/url.js
+++ server/models/url.js
@@ -4,10 +4,11 @@ const Schema = mongoose.Schema
 const UrlSchema = new Schema({
   short: { type: String, required: true, unique: true },
   enhanced: { type: String, required: true, unique: true },
+  //to do type date...
 })
 
 // Url.methods.getInfo = function() {
 //   return this.short + ' ' + this.enhanced
 // }
 
-module.exports = mongoose.model('Url', UrlSchema)
+module.exports = mongoose.model('Urlprocessed', UrlSchema)
