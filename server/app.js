b4513e8bc0f7ead4425822acd0c8cc5aa531dc32
Manuel Birba <manuelbirba@hotmail.fr>
2019-03-01 09:21:50 +0100

    [SERVER] review to perform cloud behaviour

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const chalk = require('chalk') // add some cool color to your log
 
 const mongoose = require('mongoose') //Edit data format to insert in database
 
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser') //to do : add multer for more fct in parsing data from request
 const assert = require('assert') //library to do unit test
 const flash = require('connect-flash') //middleware connect-flash : messages temporaires
 
@@ -69,7 +69,8 @@ app.get('/wrongurl', handleBadUrl)
 app.get('/me', me)
 app.post('/shorturl', shorturl)
 // app.get('/:', reverseShorturl)
-app.get('/showAllcollection', showAllcollection)
+app.get('/showallcollection', showAllcollection)
+app.post('/findoriginalurl', findOriginalUrl)
 app.get('/redirecttooriginalurl', redirectToOriginalUrl)
 app.post('/login', handleLogin)
 /*****************************************        End ROUTING    *****************************************/
@@ -94,7 +95,7 @@ function handleBadUrl(req, res) {
   res.status(400).json({ error: 'Is not a valid url' }) //http code 400 => bad parameter
 }
 
-//simule timer
+//simule timing when try to find if given URL has already been perform
 function takeTime(sec) {
   const secondes = 1000 * sec
   const time = setTimeout(function() {
@@ -104,23 +105,34 @@ function takeTime(sec) {
 }
 
 //handle perform short url functionnaly
-async function shorturl(req, res) {
-  if (!req.query) {
+async function shorturl(req, res, next) {
+  if (!req.body) {
     res.redirect('/wrongurl')
   }
   try {
-    console.log(req.query)
-    const task = await Promise.all([takeTime(10), takeTime(20), takeTime(30)])
-    //handle 'url' property
-    const resulat = `http://localhost:8000/${urlshortener.short(req.body.url)}`
-    console.log(resulat)
+    //const task = await Promise.all([takeTime(10), takeTime(20), takeTime(30)])
+
+    //handle 'url' application server property, should keep hostname
+    const reqUri =
+      `${req.protocol}://${req.get('host')}${req.originalUrl}` ||
+      'http://localhost:8000/'
+    const resulat = `${reqUri}${urlshortener.short(req.body.url)}`
+    const isOk = await verifyIfOriginalURLAlreadyShorted(req.body.url)
+    // console.log(isOk)
+    const time = 'time'
+    console.log(time)
+    // if (isOk) {
+    //   saveUrlRequest(req.body.url)
+    // }
+
     res.status(200).json({
       url: req.body.url,
       shortURL: resulat,
     })
     //res.status(200).json({ succes: 'try to read url short link in db' })
   } catch (err) {
-    req.flash('error', `Impossible d’afficher l' url demandée : ${err.message}`)
+    //req.flash('error', `Impossible d’afficher l' url demandée : ${err.message}`)
+    console.log('before redirect to home')
     res.redirect('/')
   }
 }
@@ -136,25 +148,64 @@ function showAllcollection(req, res) {
   console.log('try show db')
 }
 
-//show all url store in database
-function verifyIfURLAlreadyShorted(pUrl) {
+/***BEGIN To do in the part below => one unique fonction to verify existence of original and short url***/
+//verify is original url is store in database
+async function verifyIfOriginalURLAlreadyShorted(pUrl) {
+  //set shorted format (URL to URN)
+  //console.log(pUrl, 'purl original!!!')
+  const isOk = await Url.find({ url: pUrl }, function(err, success) {
+    if (err) throw err
+    else {
+      if (success.length === 0) {
+        //url non en base => save it !
+        saveUrlRequest(pUrl)
+      }
+      console.log(success, 'original url in already in bdd!!!')
+      return success
+    }
+  })
+
+  console.log(isOK)
+  const time = 'time'
+  console.log(time)
+  return isOk
+}
+
+//verify all urlshort store in database
+async function verifyIfURLAlreadyShorted(pUrl) {
   //set shorted format (URL to URN)
   const urn = pUrl
-  Url.find({ enhanced: urn }, function(err, url) {
+  console.log(purl, 'purl !!!')
+  await Url.find({ enhanced: urn }, function(err, url) {
     if (err) throw err
-    console.log(url)
-    res.redirect('/redirecttooriginalurl')
+    console.log(url, 'verified url in fct!!!')
+    return url
   })
 }
-verifyIfURLAlreadyShorted('46aaab2081983c4553980869b98d6dd0')
+/***END To do in the part below => one unique fonction to verify existence of original and short url***/
 
-function redirectToOriginalUrl(params) {}
+async function findOriginalUrl(req, res, next) {
+  // await verifyIfURLAlreadyShorted('46aaab2081983c4553980869b98d6dd0')
+  console.log(req.body, 'url shorted')
+  const isok = await verifyIfURLAlreadyShorted(req.body.urlshorted)
+  console.log(isok)
+  const time = 'time'
+  console.log(time)
+  // req.flash('info', 'given short url match!')
+  res.redirect('/redirecttooriginalurl')
+}
+
+function redirectToOriginalUrl(req, res, next) {
+  const reqFlash = req.flash('info')
+  console.log(reqFlash, 'redirection done')
+  res.status(200).json({ message: reqFlash })
+}
 //get some info from me 😃
 function me(req, res) {
   res.status(200).json({
     success: "It's Manuel Birba API",
     date: '26/02/2019',
-    goal: 'succed Amber Test',
+    goal: 'passing the Amber Test',
   })
 }
 /*****************************************        End Handle ROUTING    *****************************************/
